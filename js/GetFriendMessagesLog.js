3302b65d20a46a6fe08f7c8ea8887bb66f3bcead
MalikAQayum <l_malik_l@hotmail.com>
2021-03-31 05:48:41 +0200

    GetFriendMessagesLog

---
+++ js/GetFriendMessagesLog.js
@@ -0,0 +1,19 @@
+//GetFriendMessagesLog.json
+$.ajax({
+   url: "https://raw.githubusercontent.com/MalikSteam/account_public/main/private/GetFriendMessagesLog.json",
+	type:"get",
+	dataType: 'text',  
+	error: function(data){
+	//debugger;
+	  //alert('err');
+	},
+	success:function(data) {
+		var data = JSON.parse( data );
+		
+		var chatlog = unescape(data.html);
+		$(".chatlog").append(chatlog);
+		
+		$('#chatlog_table').DataTable();
+		$("#chatlog_table_wrapper > div:nth-child(3) > div.col-sm-12.col-md-5").hide();
+	}
+});
